0fb0fbd4b1aae81e197d1e6ef5ad91eb3b3ce216
TruongDX8 <truonghusk17@gmail.com>
2020-04-05 09:07:48 +0700

    Add Recycler View

---
+++ app/src/main/java/com/truongdx8/myapplication/BookStore.java
@@ -0,0 +1,14 @@
+package com.truongdx8.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookStore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_store);
+    }
+}
