1ea5772e50695d862d3b2128933a4f037e818b19
TruongDX8 <truonghusk17@gmail.com>
2020-04-06 19:44:26 +0700

    B6: JSon Demo: Add lib

---
+++ app/src/main/java/com/truongdx8/myapplication/JsonDemo.java
@@ -0,0 +1,15 @@
+package com.truongdx8.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JsonDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_json_demo);
+
+    }
+}
